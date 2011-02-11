b9acffbdb5bb6f09e82f2ad6855bdaa7a051d410
Forkin <forkin@megatron.cidadelas.com.br>
2011-02-11 13:58:57 -0200

    Estudo JQuery + Rails 3. Rafael Tauil

---
+++ todo-list/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = '564185b0ceed07e545c11383306423ab2e586b42a9f95f4d70594e73cb5ceb0044e5476d1b5e6d9f139ce6ed1fec20b819008ce47348f457dd5bbf1cc2b3fe45'
